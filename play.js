f3f1332c3adf30cc9d11126282dde6bfa20152bd
amrendrakind <vision2010@gmail.com>
2022-01-23 14:02:04 +0530

    Set Interval

--- play.js
+++ play.js
@@ -5,5 +5,19 @@ const person = {
         console.log("Hii, I am ", this.name)
     }
 }
-
 person.greet()
+
+const hobbies = ["Sports", "Cooking"]
+
+console.log(hobbies.map(hobby => "Hobby: "+hobby))
+console.log(hobbies)
+
+
+const printNumbersOnInterval = (n) => {
+    for (let i = 1; i <= n; i++) {
+      setTimeout(() => {
+        console.log(i)
+      }, i*1000)
+    }
+  }
+  printNumbersOnInterval(10);
