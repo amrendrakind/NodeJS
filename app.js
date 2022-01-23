05fbe6ce20068bde7738fff7372a3cd1378c6b3d
amrendrakind <vision2010@gmail.com>
2022-01-24 01:09:21 +0530

    Response from Server

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require("http")
 function rqListener (req, res){
-    console.log(req.url, req.method, req.headers)
+
+    const url = req.url
+    if(url ==='/'){
+
+        res.write("<html>");
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method ="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    
+    }
+
     res.setHeader("content-type", 'text/html')
     res.write("<html>");
     res.write('<head><title>My Fist Page from Server</title></head>')
