dd6ebdcc14ad36d3038a6875ceebe3b1b5d54bc8
amrendrakind <vision2010@gmail.com>
2022-01-24 09:59:09 +0530

    Response from Server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http")
+const fs = require('fs')
 function rqListener (req, res){
 
     const url = req.url
+    const method = req.method 
     if(url ==='/'){
 
         res.write("<html>");
@@ -9,7 +11,25 @@ function rqListener (req, res){
         res.write('<body><form action="/message" method ="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
         res.write('</html>')
         return res.end()
-    
+    }
+
+    if(url ==='/message' && method ==='POST'){
+
+        const body = []
+        req.on('data', (chunk)=>{
+            console.log(chunk)
+            body.push(chunk)
+        })
+
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
+
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
     }
 
     res.setHeader("content-type", 'text/html')
