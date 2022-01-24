74e106f8559ca1545277a61e14ac872e8f80000e
amrendrakind <vision2010@gmail.com>
2022-01-24 11:44:10 +0530

    Response from Server

--- app.js
+++ app.js
@@ -17,19 +17,18 @@ function rqListener (req, res){
 
         const body = []
         req.on('data', (chunk)=>{
-            console.log(chunk)
             body.push(chunk)
         })
 
-        req.on('end',()=>{
+        return req.on('end',()=>{
             const parsedBody = Buffer.concat(body).toString()
             const message = parsedBody.split('=')[1]
-            fs.writeFileSync('message.txt', message)
+            fs.writeFile('message.txt', message, (err)=>{
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+            })
         })
-
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
     }
 
     res.setHeader("content-type", 'text/html')
