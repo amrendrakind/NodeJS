fb1d104929a12f0d19386b5f10a8b9a40f10a19f
amrendrakind <vision2010@gmail.com>
2022-01-24 13:19:34 +0530

    Routes added

--- app.js
+++ app.js
@@ -1,44 +1,7 @@
 const http = require("http")
-const fs = require('fs')
-function rqListener (req, res){
+const routes = require('./routes')
 
-    const url = req.url
-    const method = req.method 
-    if(url ==='/'){
-
-        res.write("<html>");
-        res.write('<head><title>Enter Message</title></head>')
-        res.write('<body><form action="/message" method ="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-        res.write('</html>')
-        return res.end()
-    }
-
-    if(url ==='/message' && method ==='POST'){
-
-        const body = []
-        req.on('data', (chunk)=>{
-            body.push(chunk)
-        })
-
-        return req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split('=')[1]
-            fs.writeFile('message.txt', message, (err)=>{
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            })
-        })
-    }
-
-    res.setHeader("content-type", 'text/html')
-    res.write("<html>");
-    res.write('<head><title>My Fist Page from Server</title></head>')
-    res.write('<body><h1>Hello!!!!! Welcome from the Node JS Server</h1></body>')
-    res.write('</html>')
-    res.end()
-}
-const server = http.createServer(rqListener);
+const server = http.createServer( routes);
 
 PORT = 5000
 server.listen(PORT,console.log(`Server is listening at Port ${PORT}`));
---
+++ routes.js
@@ -0,0 +1,42 @@
+    const fs = require('fs')
+
+    const requestHandler = (req,res)=>{
+        const url = req.url
+        const method = req.method
+
+        if(url ==='/'){
+
+            res.write("<html>");
+            res.write('<head><title>Enter Message</title></head>')
+            res.write('<body><form action="/message" method ="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+            res.write('</html>')
+            return res.end()
+        }
+    
+        if(url ==='/message' && method ==='POST'){
+    
+            const body = []
+            req.on('data', (chunk)=>{
+                body.push(chunk)
+            })
+    
+            return req.on('end',()=>{
+                const parsedBody = Buffer.concat(body).toString()
+                const message = parsedBody.split('=')[1]
+                fs.writeFile('message.txt', message, (err)=>{
+                    res.statusCode = 302
+                    res.setHeader('Location', '/')
+                    return res.end()
+                })
+            })
+        }
+    
+        res.setHeader("content-type", 'text/html')
+        res.write("<html>");
+        res.write('<head><title>My Fist Page from Server</title></head>')
+        res.write('<body><h1>Hello!!!!! Welcome from the Node JS Server</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+module.exports = requestHandler
+    
