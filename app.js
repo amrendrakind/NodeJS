f98fd7b2cd9096f8bed53dff156757aa1c5bc507
amrendrakind <vision2010@gmail.com>
2022-01-25 08:34:16 +0530

    Express Middleware

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ app.use((req, res, next)=>{
 
 app.use((req, res, next)=>{
     console.log("In the another Middleware")
-    res.send('<h1>Hellow from Express!!!</h1>')
+    res.send('<h1>Hello from Express!!!</h1>')
 })
 
-const server = http.createServer(app)
 const PORT = 4000
-server.listen(PORT, console.log(`Server is running on ${PORT}`))
+app.listen(PORT, console.log(`Server is running on ${PORT}`))
